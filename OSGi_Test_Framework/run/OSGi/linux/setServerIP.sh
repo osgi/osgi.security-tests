bfe2d168309124060e5fff7b94b239c7f8669556
Romain Siche <romain.siche@sogeti.com>
2016-03-11 15:13:17 +0100

    003: Delete OSGi_Security_Framework and OSGi_Test_Framework directories
    Signed-off-by: Romain Siche <romain.siche@sogeti.com>

--- OSGi_Test_Framework/run/OSGi/linux/setServerIP.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ROOT="$DIR/.."
-
-echo -e "# Standard output configuration\ndisplay.box=true\ndisplay.server=true\n\n# Server configuration\nserver.ip=$1\nserver.port=2009\nserver.dport=2010\nserver.sport=2011\n\n# Automated testing\ntest=true\n" > $ROOT/com.sogetiht.otb.properties.cfg
-
-echo -e "Server IP address set to $1\n"
--- OSGi_Test_Framework/run/OSGi/windows/runServer.bat
+++
@@ -1,5 +0,0 @@
-echo off
-cls
-
-cd ../../../tests/bundles/server/
-java Server
