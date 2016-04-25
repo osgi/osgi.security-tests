1b022b2dff58b147f2204b49e6a895d228be0f07
MAINAND <thibault.mainand@sogeti.com>
2016-04-25 11:09:21 +0200

    010 : add counter for bundle 175 and log system for server and util
    Signed-off-by: Thibault Mainand <thibault.mainand@sogeti.com>
    
    011: Add bundle sysatt175 with JUnit + util and server bundles updates (log system)
    
    Signed-off-by: Romain Siche <romain.siche@sogeti.com>
    
    010: Add bundle sysatt175 with JUnit + util and server bundles updates (log system)
    
    Signed-off-by: Romain Siche <romain.siche@sogeti.com>

--- org.osgi.security.server.provider/src/org/osgi/security/server/provider/Activator.java
+++ org.osgi.security.server.provider/src/org/osgi/security/server/provider/Activator.java
@@ -5,19 +5,22 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.PrintStream;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.File;
 
 import java.net.Socket;
 import java.net.ServerSocket;
 import java.net.InetAddress;
-
+import java.util.Properties;
 import java.util.Scanner;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-
 import org.osgi.service.component.annotations.Component;
 
 /**
@@ -27,13 +30,21 @@ import org.osgi.service.component.annotations.Component;
 @Component(name = "org.osgi.security.server")
 public class Activator implements BundleActivator
 {
-    static int port = 2009, sport = 2010, dport = 2011;
+    private static int port = 2009, sport = 2010, dport = 2011;
+	private static String ftdPath = "generated/transferredFiles";
 	private static Scanner sc;
+	
+	//To write log file
+	private FileWriter writer = null;
 	
 	public void start(BundleContext context) throws Exception
 	{
-		Thread t = new Thread() {
-			
+		
+		//method to redirect the output System.out and System.err
+		outputRedirection();
+		
+		Thread t = new Thread() 
+		{	
 			public void run()
 			{
 				ServerSocket ss, ss1;
@@ -302,7 +313,13 @@ public class Activator implements BundleActivator
 
             if (splitCmd[0].equals("SENDING_FILE"))
             {
-                outStream = new FileOutputStream(splitCmd[1]);
+            	File ftdPathDirectory = new File(ftdPath);
+            	//if directory does not exist
+            	if(!ftdPathDirectory.exists())
+            	{
+            		ftdPathDirectory.mkdirs();
+            	}
+                outStream = new FileOutputStream(ftdPath + System.getProperty("file.separator") + splitCmd[1]);
                 do
                 {
                     bytesRead = is.read(buffer, 0, buffer.length);
@@ -328,11 +345,121 @@ public class Activator implements BundleActivator
             // System.out.println("Error on downloading file!");
         }
     }
+    
+    
+    private void getServerProperties()
+    {
+    	Properties prop = new Properties();
+		// pour test sur felix (RPI)
+		// File file = new File("com.sogetiht.otb.properties.cfg");
+		// Pour test sur livebox
+		File file = new File(System.getProperty("user.dir") + System.getProperty("file.separator") + "org.osgi.security.properties.cfg");
+		try
+		{
+			//load a property file
+			InputStream is = new FileInputStream(file);
+			prop.load(is);
+			is.close();
+		}
+		catch (FileNotFoundException e)
+		{
+			System.err.println("ERROR: Could not load property file '"
+					+ file.getAbsolutePath()
+			        + "'. The system cannot find the file specified.");
+		}
+		catch (Exception e)
+		{
+			System.err.println(e);
+			e.printStackTrace();
+		}
+		
+		try
+		{
+			//get the property values
+			port          = Integer.parseInt(getProp(prop,"server.port","2009"));
+			dport         = Integer.parseInt(getProp(prop,"server.dport","2010"));
+			sport         = Integer.parseInt(getProp(prop,"server.sport","2011"));
+			ftdPath	      = getProp(prop, "server.tfdPath", "generated/transferredFiles/");
+		}
+		catch (Exception e)
+		{
+			System.err.println(e);
+			e.printStackTrace();
+		}
+    }
+    
+    private void outputRedirection()
+    {
+		//Create and erase previous log file
+		try 
+		{
+			writer = new FileWriter("generated/Log.txt");
+		} 
+		catch (IOException e1) 
+		{
+			e1.printStackTrace();
+		}
+		
+		PrintStream stream = new PrintStream(System.out)
+		{			
+			public void println(String str)
+			{
+				super.print(str + System.getProperty("line.separator"));
+				try 
+				{
+					writer.append(str, 0, str.length());
+					writer.append(System.getProperty("line.separator"));
+					writer.flush();
+				} 
+				catch (IOException e) 
+				{
+					e.printStackTrace();
+				}
+			}
+			
+			public void print(String str)
+			{
+				super.print(str);
+				try 
+				{
+					writer.append(str, 0, str.length());
+					writer.flush();
+				} catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+			
+		};
+		
+		System.setOut(stream);
+		System.setErr(stream);
+	}
+    
+    private String getProp(Properties prop, String key, String defaultValue)
+	{
+		String property = null;
+		try
+		{
+			property = prop.getProperty(key);
+		}
+		catch (ClassCastException e)
+		{
+			System.err.println("ERROR: The property ("+key+") contains any key or value that isn't a string. The default property ("+defaultValue+") is set");
+		}
+   
+		if (property == null || property.equals(""))
+		{
+			property = defaultValue;
+		}
+		
+		return property;
+	}
+    
 
 	@Override
 	public void stop(BundleContext context) throws Exception
 	{
 		// TODO Auto-generated method stub
-		
 	}
 }
